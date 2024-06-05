932362ba8e227cb0d4f98aeb5493542179722a38
Waqac <waqas.sd36@gmail.com>
2024-06-05 18:21:46 +0500

    adding new features

--- app/src/main/java/com/example/emanager/MainActivity.java
+++ app/src/main/java/com/example/emanager/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.emanager;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.emanager.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolBar);
+        getSupportActionBar().setTitle("Transaction");
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.top_menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
